74e8602bc72de70c5edae74741b0468c8742d9b7
r3j0 <readytojoin086@gmail.com>
2025-09-30 20:16:57 +0900

    feat: Add Project Files

---
+++ src/main/java/org/example/SimpleTodoApplication.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTodoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleTodoApplication.class, args);
+    }
+}
