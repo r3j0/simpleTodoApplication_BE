17bd433c89994d4814cb858a66dca53bbdf498a3
rejo <readytojoin086@gmail.com>
2025-10-11 22:51:41 +0900

    fix: Allow localhost

--- src/main/java/org/example/simpleTodo/Config/WebConfig.java
+++ src/main/java/org/example/simpleTodo/Config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         // 모든 요청에 대해 CORS 허용
         registry.addMapping("/**")
-                .allowedOrigins("*")  // 모든 origin 허용
+                .allowedOrigins("http://localhost:8080")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // 허용할 HTTP 메서드
                 .allowedHeaders("*")  // 모든 헤더 허용
                 .allowCredentials(true) // 쿠키나 인증정보 허용 여부
