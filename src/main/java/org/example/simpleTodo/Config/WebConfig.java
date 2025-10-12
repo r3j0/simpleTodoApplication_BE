4af97d0584bc7ad92602683587f0e59500a810b1
r3j0 <readytojoin086@gmail.com>
2025-10-12 19:17:44 +0900

    fix: Edit WebConfig

--- src/main/java/org/example/simpleTodo/Config/WebConfig.java
+++ src/main/java/org/example/simpleTodo/Config/WebConfig.java
@@ -1,31 +1,27 @@
 package org.example.simpleTodo.Config;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    private final Environment env;
+
+    public WebConfig(Environment env) {
+        this.env = env;
+    }
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        
-        @Value("${WEB_URL}")
-        protected String webUrl;
+        String webUrl = env.getProperty("web.url", "http://localhost:8080");
         
         // 모든 요청에 대해 CORS 허용
         registry.addMapping("/**")
                 .allowedOrigins(webUrl)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
-                .allowedHeaders(
-                        "Content-Type",     // JSON 데이터
-                        "Authorization",    // JWT 또는 인증 토큰
-                        "Accept",           // 응답 타입
-                        "X-Requested-With", // Ajax
-                        "Origin",           // CORS
-                        "Access-Control-Allow-Headers",  // CORS
-                        "Access-Control-Allow-Origin"   // CORS
-                )
+                .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
     }
